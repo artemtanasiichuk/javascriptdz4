8adc0e9f88acec93cbd7a18087fdcac89b4702c4
artemtanasiichuk <aizensama2356@gmail.com>
2025-07-21 17:58:20 +0200

    .

---
+++ index.js
@@ -0,0 +1,55 @@
+let textField1 = "text1";
+let textField2 = "text2";
+if (textField1 && textField2) {
+  console.log("oba polya full");
+} else {
+  console.log("ne vse full"); 
+}
+
+
+
+
+let number1 = 5; 
+let number2 = 7;
+let sum = numberone + numbertwo;
+if (sum > 10) {
+  console.log("symma bolshe 10");
+} else {
+  console.log("symma menshe 10");
+}
+
+
+
+
+let text = "y trexta est JavaScript"; 
+if (text.includes) {
+  console.log("text imeet JavaScript");
+} else {
+  console.log("text ne imeet slov JavaScript");
+}
+
+
+
+
+let num = 15; 
+if (num > 10 && num < 20) {
+  console.log("chislo vhodit v deapazon od 10 до 20");
+} else {
+  console.log("chislo ne vhodit v deapazon od 10 до 20");
+}
+
+
+
+
+let name = "Anna"; //5
+let email = "anna32423423@example.com";
+let password = "532523325545";
+let isNameValid = name.length >= 3;
+let isEmailValid =
+  email.includes && email.indexOf > email.indexOf;
+let isPasswordValid = password.length >= 6;
+if (isNameValid && isEmailValid && isPasswordValid) {
+  console.log("perenapravlenya v druhu storinku");
+} else {
+  console.log("pshibky ne pravilno zapolnili");
+}
